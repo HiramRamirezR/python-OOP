163a535ce827e10070be8a88c9d90ca2deb724bd
hiramramirez <hiram.j.ramirez@gmail.com>
2023-08-16 20:31:24 -0500

    little fix

---
+++ 03/test.py
@@ -0,0 +1,15 @@
+# Start the name of your classes with uppercase!
+
+class User:
+    age = 0
+
+hiram = User()
+lucy = User()
+
+hiram.age = 33
+
+print(hiram)
+print(lucy)
+
+print(hiram.age)
+print(lucy.age)
