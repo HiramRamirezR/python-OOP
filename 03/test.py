d9b0f54d6a70f38749501deb0cf1844c3dbe2ca4
hiramramirez <hiram.j.ramirez@gmail.com>
2023-08-17 01:51:55 -0500

    practice sintaxys

--- 03/test.py
+++ 03/test.py
@@ -4,12 +4,36 @@ class User:
     age = 0
 
 hiram = User()
+
+
 lucy = User()
 
 hiram.age = 33
 
-print(hiram)
-print(lucy)
-
 print(hiram.age)
 print(lucy.age)
+
+
+class Monitor:
+    width = 1080
+    height = 720
+
+monitor1 = Monitor()
+monitor2 = Monitor()
+
+monitor2.width = 1920
+
+print(monitor1.width)
+print(monitor2.width)
+
+
+class Car:
+    color = 'red'
+
+car1 = Car()
+car2 = Car()
+
+car2.color = 'blue'
+
+print(car1.color)
+print(car2.color)
